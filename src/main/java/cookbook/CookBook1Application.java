285efd285b7973777e46160ec65461ac7727a52c
Cy3iu <cy3iuu@gmail.com>
2022-07-17 11:52:50 +0200

    project initi

--- src/main/java/cookbook/CookBook1Application.java
+++ src/main/java/cookbook/CookBook1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CookBook1Application {
+public class    CookBook1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(CookBook1Application.class, args);
